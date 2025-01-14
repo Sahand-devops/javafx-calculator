131408dde2f5e63e2fdfe9f5fa8a3fdfe7a13731
Snisen-dev <johan.snis@hotmail.com>
2025-01-14 13:30:10 +0100

    build: add function for app and edit .pom file

--- calculator/src/main/java/com/friends/App.java
+++ calculator/src/main/java/com/friends/App.java
@@ -1,13 +1,34 @@
 package com.friends;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Load the .fxml file
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/calculator.fxml"));
+
+        // Get the root node (Parent)
+        Parent root = fxmlLoader.load();
+
+        // Create a Scene with the loaded FXML
+        Scene scene = new Scene(root);
+
+        // Set up the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("My JavaFX App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
